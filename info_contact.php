d582ff95d04b2feb3cf04775101b18ac32a9039a
CarTon7 <120257628+CarTon7@users.noreply.github.com>
2023-02-03 11:15:19 +0100

    Add files via upload

---
+++ info_contact.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['Valider'])
+echo $_POST['email'];
+echo $_POST['lastname'];
+echo $_POST['firstname'];
+echo $_POST['sexe'];
+echo $_POST['textarea'];
+print $_POST['email'];
+print $_POST['lastname'];
+print $_POST['firstname'];
+print $_POST['sexe'];
+print $_POST['textarea'];
+?>
